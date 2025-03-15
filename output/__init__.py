0af2bd7af957a15f164624a9cfa88c7d0954fa0d
Ross Virostko <rvirostko@icloud.com>
2025-03-14 20:28:14 -0400

    Added redirector feature for use with Open/Close

--- output/__init__.py
+++ output/__init__.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 from .base import RecordWriter, FileRecordWriter, DelegatingRecordWriter
 from .csv import CSVRecordWriter
 from .json import JSONRecordWriter
@@ -5,5 +7,17 @@ from .markdown import MarkdownRecordWriter
 from .cartesian_product import RecordCartesianProduct
 from .limiter import RecordLimiter
 from .template import TemplateRecordWriter
+from .redirector import IORedirector
 
-__all__ = ["CSVRecordWriter", "DelegatingRecordWriter", "FileRecordWriter", "JSONRecordWriter", "MarkdownRecordWriter", "RecordCartesianProduct", "RecordLimiter", "RecordWriter", "TemplateRecordWriter"]
+__all__ = [
+    "CSVRecordWriter",
+    "DelegatingRecordWriter",
+    "FileRecordWriter",
+    "IORedirector",
+    "JSONRecordWriter",
+    "MarkdownRecordWriter",
+    "RecordCartesianProduct",
+    "RecordLimiter",
+    "RecordWriter",
+    "TemplateRecordWriter",
+    ]
