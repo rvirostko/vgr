9a0e8f950f4c9b1625aa3335ef75a27e28f7fc9b
Ross Virostko <rvirostko@icloud.com>
2025-03-21 19:03:39 -0400

    Updated for changes to output config and expanded.

--- experiments/output_test.py
+++ experiments/output_test.py
@@ -8,20 +8,41 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from output import JSONRecordWriter, CSVRecordWriter, MarkdownRecordWriter, RecordLimiter, RecordCartesianProduct, RecordWriter, TemplateRecordWriter
 
-def main():
+def create(output_type, encode_ascii, headers, omit_headers, compact, include_null, indent, sort_keys):
+    if output_type == 'markdown':
+        return MarkdownRecordWriter(encode_ascii=encode_ascii,
+                                    headers=headers,
+                                    omit_headers=omit_headers
+                                    )
+    if output_type == 'template':
+        return TemplateRecordWriter(encode_ascii=encode_ascii,
+                                    headers=headers,
+                                    omit_headers=omit_headers
+                                    )
+    if output_type == 'csv':
+        return CSVRecordWriter(encode_ascii=encode_ascii,
+                               headers=headers,
+                               omit_headers=omit_headers
+                               )
+        # TODO fix
+        #out.quoting = out.quoting
+        #out.quotechar = out.quotechar
+        #out.lineterminator = out.lineterminator
+        #out.set_escapechar = out.escapechar
+    if output_type.startswith('json'):
+        return JSONRecordWriter(encode_ascii=encode_ascii,
+                                headers=headers,
+                                omit_headers=omit_headers,
+                                compact=compact,
+                                include_null=include_null,
+                                indent=indent,
+                                sort_keys=sort_keys,
+                                root='results' if output_type == 'json-root' else None,
+                                array_wrapper = output_type != 'json-line'
+                                )
+    raise ValueError(output_type)
 
-    # These would be in the data from the select statement's parse tree
-    offset = 0#4
-    limit = None#4
-    encode_ascii = False#True
-    include_null = False#True
-    compact = True
-    indent = 2
-    sort_keys = True
-    # The data that would come back from the query
-    # TODO need to test output with single dict
-    headers = [ "name", "age", "pos" ]
-    product = [False, False, False]
+def main():
     data = [
         ["Alice", 25, "Engineer"],
         ["Bob", 30, "Doctor"],
@@ -31,51 +52,29 @@ def main():
         ["Limbo", None, ["Hobo", "流浪"]],
         ["Complex", 99, {'a': 1, 'b': [2,3]}]
     ]
-
+    headers = [ "name", "age", "pos" ]
     for output_type in ('markdown', 'json-array', 'json-root', 'json-line', 'csv', 'template'):
-        out: RecordWriter = None
-        if output_type == 'markdown':
-            out = (MarkdownRecordWriter().
-                    set_encode_ascii(encode_ascii).
-                    set_headers(headers))
-        elif output_type == 'template':
-            out = (TemplateRecordWriter().
-                   set_encode_ascii(encode_ascii).
-                   set_headers(headers))
-        elif output_type == 'csv':
-            out = (CSVRecordWriter().
-                    set_encode_ascii(encode_ascii).
-                    set_headers(headers).
-                    set_quoting().
-                    set_quotechar().
-                    set_lineterminator().
-                    set_escapechar())
-        elif output_type.startswith('json'):
-            out = (JSONRecordWriter().
-                    set_encode_ascii(encode_ascii).
-                    set_headers(headers).
-                    set_compact(compact).
-                    set_include_null(include_null).
-                    set_indent(indent).
-                    set_sort_keys(sort_keys))
-            if output_type == 'json-root':
-                out.set_root('results')
-            if output_type == 'json-line':
-                out.set_exclude_array_wrapper()
-        else:
-            raise ValueError(output_type)
-
-        # Order is important since projections can generate more than one row
-        # It depends upon how you want to interpret the limit/offset, and that is TBD
-        out = RecordLimiter.wrap(out, limit, offset)
-        out = RecordCartesianProduct.wrap(out, product)
-
-        print(f'{output_type}-')
-        if out.start():
-            for row in data:
-                if not out.write(row): break
-            out.finish()
-        print()
+        for omit_headers in [True, False]:
+            for encode_ascii in [True, False]:
+                for offset in [0, 2]:
+                    for limit in [None, 4]:
+                        for include_null in [True, False]:
+                            for compact in [True, False]:
+                                for indent in [0, 2, 8]:
+                                    for sort_keys in [True, False]:
+                                        for product in [[False, False, False], [False, False, True], [False, True, True]]:
+                                            out: RecordWriter = create(output_type=output_type, encode_ascii=encode_ascii, headers=headers, compact=compact, include_null=include_null, indent=indent, sort_keys=sort_keys, omit_headers=omit_headers)
+                                            # Order is important since projections can generate more than one row
+                                            # It depends upon how you want to interpret the limit/offset, and that is TBD
+                                            out = RecordLimiter.wrap(out, limit=limit, offset=offset)
+                                            out = RecordCartesianProduct.wrap(out, product)
+                                            print(repr(out), flush=True)
+                                            print(f'{output_type}-', flush=True)
+                                            if out.start():
+                                                for row in data:
+                                                    if not out.write(row): break
+                                                out.finish()
+                                            print(flush=True)
 
 if __name__ == "__main__":
     main()
