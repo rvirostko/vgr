529e3ad880dd62cadefa4f2276898afe3bb6b184
Ross Virostko <rvirostko@icloud.com>
2025-03-24 11:24:25 -0400

    Fix bug: where Open failed on file names that had no directory components. Improved docs.

--- output/util.py
+++ output/util.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
+"""
+Utilities for dealing with file names and paths
+"""
 
 import os
 
 def verify_relative_path(filename: str) -> str:
     """
-    The filename must be relativie to the current directory.
+    The filename must be relative to the current directory.
+    Returns filename unchanged.
     """
     full_path = expand_filename(filename)
     cwd = expand_filename(os.getcwd())
-    if os.path.commonpath([cwd, full_path]) != cwd: raise OSError(f'File {full_path} not relative to {cwd}')
+    if os.path.commonpath([cwd, full_path]) != cwd:
+        raise OSError(f'File {full_path} not relative to {cwd}')
     return filename
 
 def prepare_path(filename: str) -> str:
@@ -16,11 +21,14 @@ def prepare_path(filename: str) -> str:
     Creates the directory structure required for the filename.
     Only works with paths relative to the CWD
     """
-    full_path = verify_relative_path(filename)
+    full_path = expand_filename(verify_relative_path(filename))
     # Exclude last component, the file name
     dir_path = os.path.dirname(full_path)
     os.makedirs(dir_path, exist_ok=True)
     return filename
 
 def expand_filename(filename: str) -> str:
+    """
+    Returns the full path, absolute with user expansion et al.
+    """
     return os.path.realpath(os.path.abspath(os.path.expanduser(filename)))
