2c2cdb53a78fa171842a608101a6aed5523ef7cf
Ross Virostko <rvirostko@icloud.com>
2025-04-07 16:03:08 -0400

    Added control structures: if, while, foreach et al

--- src_mgr.py
+++ src_mgr.py
@@ -1,5 +1,8 @@
+"""
+Utility functions for working with the source associated with statements
+"""
 
-from lark import Tree, Token
+from lark import Tree
 
 class StatementSourceMgr:
     def __init__(self):
@@ -10,49 +13,14 @@ class StatementSourceMgr:
         self._statement_text: str = statement_text or ''
         self._origin: str = origin
 
-    def source_for(self, tree: Tree) -> str:
+    def source_for(self, tree: Tree, end_tree: Tree=None) -> str:
         if not self._statement_text or tree is None: return ''
-        start, end = self._subtree_span(tree)
-        return self._statement_text[start:end]
+        return self._statement_text[tree.meta.start_pos : tree.meta.end_pos if end_tree is None else end_tree.meta.start_pos]
 
     def origin(self) -> str:
         return self._origin
 
     def statement_text(self) -> str:
         return self._statement_text
-
-    def _subtree_span(self, node):
-        """
-        Recursively finds the start and end positions of a subtree in the source text.
-
-        Returns:
-            (start_pos, end_pos) where:
-            - start_pos is the earliest character index of any token in the subtree.
-            - end_pos is the last character index + 1 of any token in the subtree.
-            If no tokens are found, returns (None, None).
-        """
-        start_pos = None
-        end_pos = None
-
-        def traverse(n):
-            nonlocal start_pos, end_pos
-            if isinstance(n, Token):
-                if n.start_pos is not None:
-                    if start_pos is None or n.start_pos < start_pos:
-                        start_pos = n.start_pos
-                    if end_pos is None or (n.end_pos is not None and n.end_pos > end_pos):
-                        end_pos = n.end_pos
-            elif isinstance(n, Tree):
-                for child in n.children: traverse(child)
-                # Some Tree nodes have meta positions, but we can't fully trust it.
-                if hasattr(n, "meta") and n.meta:
-                    if hasattr(n.meta, "start_pos") and n.meta.start_pos is not None:
-                        if start_pos is None or n.meta.start_pos < start_pos:
-                            start_pos = n.meta.start_pos
-                    if hasattr(n.meta, "end_pos") and n.meta.end_pos is not None:
-                        if end_pos is None or n.meta.end_pos > end_pos:
-                            end_pos = n.meta.end_pos
-        traverse(node)
-        return start_pos, end_pos
 
 SSM = StatementSourceMgr()
--- stmt_exec.py
+++ stmt_exec.py
@@ -6,21 +6,149 @@ import math
 
 from lark import Lark, Tree, Token, Transformer, v_args
 
-from app_exceptions import remember_terminals
+from app_exceptions import remember_terminals, StatementBreak, StatementConinue
 from data_dict import DataDictionary
-from dd_config import dd_set_statement, dd_clear_scratch
-from evaluate import bind_operations
-from redir import print_stderr, execute_open, execute_close
+from dbg import print_tree
+from dd_config import dd_set_statement, dd_clear_scratch, dd_path
+from evaluate import bind_operations, eval_expr
+from mathpak import poly_bool, poly_list
+from redir import execute_open, execute_close, print_stderr
 from src_mgr import SSM
-from stmt_math import execute_add_to, execute_add_giving, execute_mul_by
-from stmt_math import execute_sub_from, execute_sub_giving, execute_div_into, execute_div_by
 from stmt_cflags import execute_debug, execute_echo, execute_verbose
 from stmt_exit import execute_assert, execute_exit
+from stmt_math import execute_add_to, execute_add_giving, execute_mul_by
+from stmt_math import execute_sub_from, execute_sub_giving, execute_div_into, execute_div_by
 from stmt_print import execute_exhibit, execute_print, execute_printf, execute_display_on
 from stmt_select import execute_select
-from stmt_set import execute_load_from, execute_set, execute_unset, execute_inc, execute_dec, execute_move_to
+from stmt_set import execute_load_from, execute_set, execute_unset, execute_inc, execute_dec, execute_move_to, do_set, do_unset
 from stmt_zip import execute_zip
-from dbg import print_tree
+
+def execute_break(_: DataDictionary, __: Tree) -> None:
+    """Exits the current block of statements.
+
+* Break [;]
+
+Can be used with If, Unless, While, Until, and ForEach statements
+"""
+    raise StatementBreak()
+
+def execute_continue(_: DataDictionary, __: Tree) -> None:
+    """Causes the current loop to to start again.
+
+* Continue [;]
+
+"""
+    raise StatementConinue()
+
+def execute_pass(_: DataDictionary, __: Tree) -> None:
+    """A placeholder for a statement.
+* NOP [;]
+* Pass [;]
+
+A placeholder for a statement, such as during iterative development
+which takes no action and has no side effects."""
+
+def _exec_if_else(dd: DataDictionary, statement: Tree, desired_value: bool) -> None:
+    if dd.echo:
+        print_stderr(SSM.source_for(statement, statement.children[1]))
+    if poly_bool(eval_expr(dd, bind_operations(statement.children[0]))) == desired_value:
+        for s in statement.children[1:]:
+            if s.data != 'else':
+                dispatch_statement(dd, s)
+    else:
+        last = statement.children[-1]
+        if last.data == 'else':
+            for s in last.children:
+                dispatch_statement(dd, s)
+
+def execute_if(dd: DataDictionary, statement: Tree) -> None:
+    """Conditionally execute a block of statements.
+
+* If _expression_ [Then | :] _statement_... End [;]
+* If _expression_ [Then | :] _statement_... Else [Do | :] _statement_... End [;]
+
+If the expression evaluates to True the first block of statements is executed.
+If it evaluates to False, the second block of statements, if provided, is executed.
+"""
+    _exec_if_else(dd, statement, True)
+
+def execute_unless(dd: DataDictionary, statement: Tree) -> None:
+    """Conditionally execute a block of statements.
+
+* Unless _expression [Then | :] _statement_... End [;]
+
+If the expression evaluates to False the block of statements is executed.
+"""
+    _exec_if_else(dd, statement, False)
+
+def _exec_loop(dd: DataDictionary, statement: Tree, desired_value: bool) -> None:
+    if dd.echo:
+        print_stderr(SSM.source_for(statement, statement.children[1]))
+    predicate = bind_operations(statement.children[0])
+    while True:
+        if poly_bool(eval_expr(dd, predicate)) != desired_value: return
+        try:
+            for s in statement.children[1:]: dispatch_statement(dd, s)
+        except StatementBreak:
+            return
+        except StatementConinue:
+            continue
+
+def execute_while(dd: DataDictionary, statement: Tree) -> None:
+    """Repeatedly execute a block of statements while a condition exists.
+
+* While _expression_ [Do | :] _statement_... End [;]
+
+As long as the expression evaluates to True, the block of statements is
+repeatedly executed.
+If a Break statement is encountered, looping ends regardless of the
+expression's value. If a Continue statement is encountered, statements
+following it are skipped, and the expression is checked again.
+    """
+    _exec_loop(dd, statement, True)
+
+def execute_until(dd: DataDictionary, statement: Tree) -> None:
+    """Repeatedly execute a block of statements until a condition is reached.
+
+* Until _expression_ [Do | :] _statement_... End [;]
+
+The block of statements is executed until the expression evaluates to True.
+If a Break statement is encountered, looping ends regardless of the
+expression's value. If a Continue statement is encountered, statements
+following it are skipped, and the expression is checked again.
+"""
+    _exec_loop(dd, statement, False)
+
+def execute_foreach(dd: DataDictionary, statement: Tree) -> None:
+    """Iterate over a set of values.
+
+* ForEach _variable_ In _expression_ [Do | :] _statement_.. End [;]
+
+If expression is a single value, non-_None_ value, the statements are executed
+exactly once. If a list, the statements are executed once for each item,
+including items that are _None_, and if a dictionary, the statements
+are executed once for each key/value pair.
+
+If a Break statement is encountered, iteration ends regardless of the
+number of items remaining. If a Continue statement is encountered, statements
+following it are skipped, and the loop continues with the next item.
+"""
+    if dd.echo:
+        print_stderr(SSM.source_for(statement, statement.children[2]))
+    path = dd_path(statement.children[0])
+    collection = poly_list(eval_expr(dd, bind_operations(statement.children[1])))
+    if collection is not None:
+        try:
+            for value in collection:
+                do_set(dd, value, *path)
+                try:
+                    for s in statement.children[2:]: dispatch_statement(dd, s)
+                except StatementBreak:
+                    return
+                except StatementConinue:
+                    continue
+        finally:
+            do_unset(dd, *path)
 
 # pylint: disable=invalid-name
 # disabled because we MUST have methods named the same as the tokens
@@ -64,7 +192,9 @@ SIMPLE_STATEMENT_HANDLERS = {
     'add_giving': execute_add_giving,
     'add_to': execute_add_to,
     'assert': execute_assert,
+    'break': execute_break,
     'close': execute_close,
+    'continue': execute_continue,
     'debug': execute_debug,
     'dec': execute_dec,
     'display_on': execute_display_on,
@@ -78,6 +208,7 @@ SIMPLE_STATEMENT_HANDLERS = {
     'move_to': execute_move_to,
     'mul_by': execute_mul_by,
     'open': execute_open,
+    'pass': execute_pass,
     'print': execute_print,
     'printf': execute_printf,
     'set': execute_set,
@@ -89,7 +220,12 @@ SIMPLE_STATEMENT_HANDLERS = {
 }
 
 X_STATEMENT_HANDLERS = {
+    'foreach': execute_foreach,
+    'if': execute_if,
     'select': execute_select,
+    'unless': execute_unless,
+    'until': execute_until,
+    'while': execute_while,
 }
 
 def remove_comments(input_text: str) -> str:
@@ -104,20 +240,29 @@ def execute_statements(parser: Lark, dd: DataDictionary, statement_text: str, so
     SSM.set_statement(statement_text, source)
     statements: Tree = parser.parse(statement_text)
     for statement in statements.children:
-        text = statement_text[statement.meta.start_pos : statement.meta.end_pos]
-        dd_set_statement(dd, text)
-        statement = ConstantsNormalizer().transform(statement)
-        statement = VarRefOptimizer().transform(statement)
-        handler = SIMPLE_STATEMENT_HANDLERS.get(statement.data)
-        if handler:
-            statement = bind_operations(statement)
-        else:
-            handler = X_STATEMENT_HANDLERS.get(statement.data)
-            if not handler:
-                raise NotImplementedError(f'No handler established for {statement.data}') #SNO
-        if dd.debug: print_tree(statement)
-        if dd.echo: print_stderr(text)
         try:
-            handler(dd, statement)
-        finally:
-            dd_clear_scratch(dd)
+            dispatch_statement(dd, statement)
+        except StatementBreak:
+            if dd.verbose: print_stderr('Break used outside control statement; ignored')
+        except StatementConinue:
+            if dd.verbose: print_stderr('Continue used outside control statement; ignored')
+
+def dispatch_statement(dd: DataDictionary, statement: Tree) -> None:
+    text = SSM.source_for(statement)
+    dd_set_statement(dd, text)
+    statement = ConstantsNormalizer().transform(statement)
+    statement = VarRefOptimizer().transform(statement)
+    handler = SIMPLE_STATEMENT_HANDLERS.get(statement.data)
+    if handler:
+        statement = bind_operations(statement)
+        if dd.echo: print_stderr(text)
+    else:
+        handler = X_STATEMENT_HANDLERS.get(statement.data)
+        # NB: these statements need to do their own bind
+        #     and decide what to do for "echo"
+    if dd.debug: print_tree(statement)
+    try:
+        if handler: handler(dd, statement)
+        else: raise NotImplementedError(f'No handler established for {statement.data}') #SNO
+    finally:
+        dd_clear_scratch(dd)
