a271efe0cfd35f071b126645ffc93fc6bcbf2893
rvirostko <rvirostko@icloud.com>
2025-04-15 11:23:00 -0400

    Update v.py
    Client skeleton

--- v.py
+++ v.py
@@ -1,1 +1,17 @@
 # Transfer
+__MIME_JSON: str = 'application/json'
+__UTF_8: str = 'utf-8'
+
+__VAULT_ADDR: str = ''
+__VAULT_TOKEN: str = ''
+VAULT_CONN: http.client.HTTPSConnection = None
+
+def vault_get(url: str, namespace: str='') -> Dict[str, Any]:
+    return vault_request(url, namespace, 'GET')
+
+def vault_list(url: str, namespace: str='') -> Dict[str, Any]:
+    return vault_request(url, namespace, 'LIST')
+
+def vault_post(url: str, namespace: str='', data: Dict[str, Any]=None) -> Dict[str, Any]:
+    return vault_request(url, namespace, 'POST', data)
+  
