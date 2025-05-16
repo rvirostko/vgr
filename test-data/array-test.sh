39b3f93b133179c00dca808f528a49b78a682702
Ross Virostko <rvirostko@icloud.com>
2025-05-16 16:06:02 -0400

    Syntax change requires semicolon after after print

--- test-data/array-test.sh
+++ test-data/array-test.sh
@@ -7,37 +7,37 @@ verbose true
 set a to [1,2,3]
 set b to a
 
-print
+print;
 set c to a + (b + 3)
 exhibit a, b, c
 assert a == b && a != c && b != c
 
-print
+print;
 set c to (b - 3) + a
 exhibit a, b, c
 assert a == b && a != c && b != c
 
-print
+print;
 set c to a + (b * 2)
 exhibit a, b, c
 assert a == b && a != c && b != c
 
-print
+print;
 set c to a + (b / 2)
 exhibit a, b, c
 assert a == b && a != c && b != c
 
-print
+print;
 set c to a + (b // 2)
 exhibit a, b, c
 assert a == b && a != c && b != c
 
-print
+print;
 set c to a + (b % 2)
 exhibit a, b, c
 assert a == b && a != c && b != c
 
-print
+print;
 set c to a + (b ** 2)
 exhibit a, b, c
 assert a == b && a != c && b != c
