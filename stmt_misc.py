c5eccda6cc3b9263a0d1bba0ab396b72175b445b
Ross Virostko <rvirostko@icloud.com>
2025-05-29 08:16:56 -0400

    Remove extraneous bind. Improve doc and add verbose support. Reduce max sleep time to five minutes.

--- stmt_misc.py
+++ stmt_misc.py
@@ -8,14 +8,21 @@ from lark import Tree
 
 from data_dict import DataDictionary
 from evaluate import eval_to_number
-from stmt_exec import bind_operations
+from redir import print_stderr
 
 def execute_sleep(dd: DataDictionary, statement: Tree) -> None:
-    """Sleep for a given number of seconds
+    """
+**Sleep for a given number of seconds**
 
-* Sleep [For] _expression_ [Seconds] [;]
+* Sleep [For] _expression_ [Second | Seconds] [;]
+
+Values may be floating point, e.g. .01 to delay for ten milliseconds.
+Negative and zero values are ignored. Maximum sleep time is five minutes.
 """
-    n = eval_to_number(dd, bind_operations(statement.children[0]), 'Sleep time')
-    n = min(max(n, 0), 3600)
+    n = eval_to_number(dd, statement.children[0], 'Sleep time')
+    n = min(max(n, 0), 300)
     if n > 0:
+        if dd.verbose: print_stderr('Sleeping for', n, "seconds")
         time.sleep(n)
+    else:
+        if dd.verbose: print_stderr('Sleep skipped')
