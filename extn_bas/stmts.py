58e03122f059df73d88043e338482057baa071b6
Ross Virostko <rvirostko@icloud.com>
2025-06-04 16:01:05 -0400

    Bug fix: "by" in for loop should be optional Doc improvements

--- extn_bas/stmts.py
+++ extn_bas/stmts.py
@@ -12,6 +12,7 @@ from stmt_set import execute_set
 from tags import control_statement
 from src_mgr import SSM
 from dd_config import dd_path, do_set, do_unset
+from evaluate import eval_to_number
 
 @control_statement
 def execute_do_while(dd: DataDictionary, statement: Tree) -> None:
@@ -45,19 +46,20 @@ following it are skipped, and the expression is checked again.
 
 @control_statement
 def execute_for_next(dd: DataDictionary, statement: Tree) -> None:
-    """A BASIC-style Do-Until Loop
+    """
+**A BASIC-style For-Next Loop**
 
-* For _varabile_ = _expressions_ To _expression_
+* For _varabile_ = _expressions_ To _expression_ [:]
     _statement_...
   Next [;]
-* For _varabile_ = _expressions_ To _expression_ By _expression_
+* For _varabile_ = _expressions_ To _expression_ By _expression_ [:]
     _statement_...
   Next [;]
 
 The block of statements is executed until the limit is exceeded.
-If a Break statement is encountered, looping ends regardless of the
-limit's value. If a Continue statement is encountered, statements
-following it are skipped, and looping proceeds.
+If a _Break_ statement is encountered, looping ends regardless of the
+limit's value. If a _Continue_ statement is encountered, statements
+following it are skipped and looping proceeds.
 """
     # Echo the control portion, not the statements
     if dd.echo:
@@ -67,16 +69,16 @@ following it are skipped, and looping proceeds.
     cindex += 1
     value = eval_to_number(dd, bind_operations(statement.children[cindex]), 'For-Next start')
     cindex += 1
-    end = eval_to_number(dd, bind_operations(statement.children[cindex]), 'For-next end')
+    end = eval_to_number(dd, bind_operations(statement.children[cindex]), 'For-Next end')
     cindex += 1
-    ccount = len(statement.children)
     inc = 1
-    if ccount >= 5:
+    if statement.data == 'basic_for_next':
+        inc = -1 if value > end else 1
+    else:
+        # This statement has a "by" clause
         inc = eval_to_number(dd, bind_operations(statement.children[cindex]), 'For-Next increment')
         if inc == 0: raise ValueError('For-Next increment must be non-zero')
         cindex += 1
-    else:
-        inc = -1 if value > end else 1
     if (end - value) * inc < 0:
         # NB: if end and value are the same, we don't care about the sign
         raise ValueError('Sign of For-Next increment results in infinite loop')
@@ -95,44 +97,62 @@ following it are skipped, and looping proceeds.
     finally:
         do_unset(dd, *path)
 
-from evaluate import eval_to_number
-
 def execute_exit(_: DataDictionary, __: Tree) -> None:
-    """Exits the current block of statements.
+    """
+**Exits the current block of statements**
 
 * Exit Do [;]
 * Exit For [;]
 * Exit While [;]
+
+BASIC variants of _Break_.
+Note that the scope portion of the command not respected; the _current_
+block is exited without regards to type.
 """
     raise StatementBreak()
 
 def execute_continue(_: DataDictionary, __: Tree) -> None:
-    """Causes the current loop to to start again.
+    """
+**Causes the current loop to to start again**
 
 * Continue Do [;]
 * Continue For [;]
 * Continue While [;]
+
+BASIC variants of _Continue_.
+Note that the scope portion of the command not respected; the _current_
+block is restarted without regards to type.
 """
     raise StatementContinue()
 
 def execute_let(dd: DataDictionary, statement: Tree) -> None:
-    """Assign a value to a variable.
+    """
+**Assign a value to a variable**
 
 * Let _variable_ = _expression_ [;]
+
+BASIC equivalent of _Set_
 """
     execute_set(dd, statement)
 
 def execute_troff(dd: DataDictionary, _: Tree) -> None:
-    """Turn off Tracing Mode (echo)
+    """
+**Turn off Tracing Mode**
 
 * Troff [;]
+
+BASIC equivalent of _Echo False_
 """
     dd.echo = False
     if dd.verbose: print_stderr('Trace Off')
 
 def execute_tron(dd: DataDictionary, _: Tree) -> None:
-    """Turn on Tracing Mode (echo)
+    """
+**Turn on Tracing Mode**
+
 * Tron [;]
+
+BASIC equivalent of _Echo True_
 """
     dd.echo = True
     if dd.verbose: print_stderr('Trace On')
