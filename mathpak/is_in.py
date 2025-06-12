95b43aa7d43e692b376cf245956315020bdda2ae
Ross Virostko <rvirostko@icloud.com>
2025-06-12 13:09:24 -0400

    Mostly comments on how to fix this in the future. One minor improvement.

--- mathpak/is_in.py
+++ mathpak/is_in.py
@@ -8,6 +8,24 @@ from typing import Any
 
 from .common import NoneType
 
+# Impl def and testing req
+#   * If x is a collection, should it be distributive (not use all)
+#     ["a", "b"] in "a" -> True or [True, False]
+#   * If reversed...
+#     "a" in ["a", "b"] -> True
+#   * If combined...
+#     ["a", "b"] in ["c", "a"] -> True or [[False, False], [False, True]]
+#   * For "In" and "Not In" the comma in X should be read as "Or"
+#   * "In" is read as "any X in Y", so 'Any "a" or "b" in ["c", "a"]'
+#   * "ContainsAny" is a reversal of "In"
+#     ["c", "a"] Contains ["a", "b"] -> True
+#     Read as "Does ["c", "a"] Contain Either "a" or "b"
+#   * "ContainsAll" reads as
+#     Read as "Does ["c", "a"] Contain Both "a" and "b"
+#   * We should have var args for contains to make things easier
+#     Print my_coll.ContainsAny("a", "c")
+#   * We could introduce an "either use a single array or multiple scalars" for the y value
+
 def poly_in(x: Any, y: Any) -> Any:
     return _is_in(x, y, False)
 
@@ -25,6 +43,7 @@ def poly_not_in(x: Any, y: Any) -> Any:
     # try a scalar comparison
     return not (isinstance(x, type(y)) and x == y)
 
+
 def poly_contains_any(x: Any, y: Any) -> Any:
     return _is_in(y, x, False)
 
@@ -33,8 +52,8 @@ def poly_contains_all(x: Any, y: Any) -> Any:
 
 def _is_in(x: Any, y: Any, do_all: bool) -> Any:
     if isinstance(x, (list, tuple)):
-        if do_all: return all(_is_in(x1, y, do_all) for x1 in x)
-        return any(_is_in(x1, y, do_all) for x1 in x)
+        t = (_is_in(x1, y, do_all) for x1 in x)
+        return all(t) if do_all else any(t)
     if not isinstance(x, (NoneType, bool, int, str, float)):
         raise TypeError(f'Cannot use {type(x)} with an in/contains operation')
     if isinstance(y, str):
