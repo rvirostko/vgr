d2d17e95fc92c81b143bf9732f15452a7241aff7
Ross Virostko <rvirostko@icloud.com>
2025-06-13 16:33:20 -0400

    Documentation. Div by zero no longer needs handling at this level

--- mathpak/mod.py
+++ mathpak/mod.py
@@ -1,5 +1,5 @@
 """
-Modulus operation.
+Modulo operation.
 """
 
 from functools import reduce
@@ -8,35 +8,35 @@ from typing import Any
 from .common import numeric_operations, get_operation
 
 def poly_vmod(x: Any, *args):
-    """Varargs version of poly_mod"""
-    return reduce(poly_mod, args, x)
+    """
+**Polymorphic modulo**
 
+* _x_ % _y_
+* _x_ Mod _y_
+* _x_.Mod(_y..._)
 
-def poly_mod(x: Any, y: Any) -> Any:
-    """Polymorphic modulus function.
-
-| x     | y     | returns | operation            |
-|-------|-------|---------|----------------------|
-| int   | int   | float   | x % y                |
-| int   | float | float   | x % y                |
-| int   | str   | float   | x % float(y)         |
-| float | int   | float   | x % y                |
-| float | float | float   | x % y                |
-| float | str   | float   | x % float(y)         |
-| str   | int   | float   | float(x) % y         |
-| str   | float | float   | float(x) % y         |
-| str   | str   | float   | float(x) % float(y)  |
-| list  | int   | list    | distributive         |
-| list  | float | list    | distributive         |
-| list  | str   | list    | distributive         |
-| tuple | int   | tuple   | distributive         |
-| tuple | float | tuple   | distributive         |
-| tuple | str   | tuple   | distributive         |
+| x     | y     | returns | operation           |
+|-------|-------|---------|---------------------|
+| int   | int   | float   | x % y               |
+| int   | float | float   | x % y               |
+| int   | str   | float   | x % float(y)        |
+| float | int   | float   | x % y               |
+| float | float | float   | x % y               |
+| float | str   | float   | x % float(y)        |
+| str   | int   | float   | float(x) % y        |
+| str   | float | float   | float(x) % y        |
+| str   | str   | float   | float(x) % float(y) |
+| list  | int   | list    | distributive        |
+| list  | float | list    | distributive        |
+| list  | str   | list    | distributive        |
+| tuple | int   | tuple   | distributive        |
+| tuple | float | tuple   | distributive        |
+| tuple | str   | tuple   | distributive        |
 
 TypeError raised on all other combinations
 """
+    return reduce(poly_mod, args, x)
+
+def poly_mod(x: Any, y: Any) -> Any:
     operation = get_operation(x, y, numeric_operations)
-    try:
-        return operation(poly_mod, x, y) if operation else x % y
-    except ZeroDivisionError as e:
-        raise ValueError('Modulo by Zero') from e
+    return operation(poly_mod, x, y) if operation else x % y
