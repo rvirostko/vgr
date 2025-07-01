350ebcb91338519e94dce1005f8c60d319df4540
Ross Virostko <rvirostko@icloud.com>
2025-07-01 13:34:51 -0400

    Placeholder for future functions

--- extn_term/term_extn.py
+++ extn_term/term_extn.py
@@ -13,6 +13,10 @@ from .stmts import (
     add_dd_constants
 )
 
+_FUNCTIONS = {
+    #    Future
+}
+
 _HANDLERS = {
     'term_statement': execute_term_statement
 }
@@ -31,6 +35,9 @@ class TermExtension(VgrExtension):
         extn_grammar = Path(__file__).parent / 'term.ebnf'
         with extn_grammar.open('r', encoding='utf-8') as f:
             return f.read()
+
+    def functions(self) -> Dict[str, Callable]:
+        return _FUNCTIONS
 
     def statement_handlers(self) -> Dict[str, Callable]:
         return _HANDLERS
