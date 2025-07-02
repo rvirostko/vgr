b4e6376af78dc9f5b04090911df884f6b96487b9
Ross Virostko <rvirostko@icloud.com>
2025-07-02 14:52:38 -0400

    Check point for list replace work

--- stmt_list.py
+++ stmt_list.py
@@ -12,10 +12,10 @@ from dd_config import dd_path
 from evaluate import eval_expr, eval_to_int
 from redir import print_stderr
 
-def _extract_list_all(statement: Tree) -> tuple[int, bool]:
-    if isinstance(statement.children[0], Tree) and statement.children[0].data == "list_all":
-        return 1, True
-    return 0, False
+def _extract_list_all(statement: Tree, idx: int=0) -> tuple[int, bool]:
+    if isinstance(statement.children[idx], Tree) and statement.children[0].data == "list_all":
+        return idx + 1, True
+    return idx, False
 
 def _eval_and_advance(dd: DataDictionary, statement: Tree, idx: int) -> tuple[int, Any]:
     return idx + 1, eval_expr(dd, statement.children[idx])
@@ -35,6 +35,7 @@ def _eval_list_target(dd: DataDictionary, statement: Tree, idx: int) -> tuple[in
     return idx, path, value
 
 def _eval_list_giving(statement: Tree, idx: int) -> tuple[Tree, tuple[str]]:
+    # NB: since this relies on length alone, it can be fragile
     gexpr = statement.children[idx] if idx < len(statement.children) else None
     return gexpr, dd_path(gexpr) if gexpr else None
 
@@ -120,30 +121,57 @@ def execute_list_remove_last(dd: DataDictionary, statement: Tree) -> None:
             print_stderr('List', '.'.join(path), 'unchanged')
 
 def execute_list_remove(dd: DataDictionary, statement: Tree) -> None:
-    idx, removals = _eval_and_advance(dd, statement, 0)
+    # While we allow "all" it does not change behavior
+    idx, _do_all = _extract_list_all(statement)
+    idx, positions = _eval_and_advance(dd, statement, idx)
     idx, path, dst = _eval_list_target(dd, statement, idx)
     gexpr, giving_path = _eval_list_giving(statement, idx)
-    #giving_array = isinstance(removals, (list, tuple))
-    rcount, removed = _remove_items(dst, _normalize_removals(removals))
+    rcount, removed = _remove_items(dst, _normalize_positions(positions))
     if rcount:
         if giving_path:
             # if caller only asked for one item, return one item
-            if not isinstance(removals, (list, tuple)): removed = removed[0]
+            if not isinstance(positions, (list, tuple)): removed = removed[0]
             _set_list_giving(dd, giving_path, removed, gexpr)
         if dd.verbose:
-            print_stderr('Removed', rcount, f'item{"s" if l != 1 else ""} From', '.'.join(path), 'At', pos)
+            print_stderr('Removed', rcount, f'item{"s" if rcount != 1 else ""} From', '.'.join(path))
     else:
         if giving_path:
             _set_list_giving(dd, giving_path, None, gexpr)
         if dd.verbose:
             print_stderr('List', '.'.join(path), 'unchanged')
 
-def _normalize_removals(removals) -> list:
+# All means treat as separate entities
+# 1) Replace 5 in item with "a" -- item[5] = "a"
+# 2) Replace 5 in item with All "a" -- item[5] = "a", All superflous because on single index
+# 3) Replace [5, 6] in item with "a" -- replaces both 5 and 6 with "a"
+# 4) Replace [5, 6] in item with All "a" -- Same as #3, following reasoning of #2
+# 5) Replace 5 in item with ["a", "b"] -- item[5] = ["a", "b"]; effectively the same as #1
+# 6) Replace 5 in item with All ["a", "b"] -- same as #1 and "b" is unused
+# 7) Replace [5, 6] in item with ["a", "b"] -- same as #3
+# 8) Replace [5, 6] in item with All ["a", "b"] -- item[5] = "a", item[6] = "b"
+# 8a) ...All ["a"] -- item[6] is either skipped or set to None
+# 8b) ...All ["a", "b", "c"] -- "c" is ignored
+
+# The general logic seems to be to create a zip longest between indicies values
+# But, for the source is shorter than the indicies, and you have a single value, you repeat it
+#    expr - indicies
+#    var_name - target
+#    list_all? - do_all
+#    expr - source
+#    list_giving?
+
+def execute_list_replace(dd: DataDictionary, statement: Tree) -> None:
+    idx, positions = _eval_and_advance(dd, statement, 0)
+    idx, path, dst = _eval_list_target(dd, statement, idx)
+    idx, do_all = _extract_list_all(statement, idx)
+    gexpr, giving_path = _eval_list_giving(statement, idx)
+
+def _normalize_positions(positions) -> list:
     """By the end, we should have a list filled with ints or Nones"""
-    if removals is None: return []
-    if isinstance(removals, tuple): removals = [*removals]
-    if not isinstance(removals, list): removals = [removals]
-    return [_to_int(item) for item in removals]
+    if positions is None: return []
+    if isinstance(positions, tuple): positions = [*positions]
+    if not isinstance(positions, list): positions = [positions]
+    return [_to_int(pos) for pos in positions]
 
 def _to_int(item: Any) -> int:
     if isinstance(item, int): return item
@@ -156,21 +184,21 @@ def _to_int(item: Any) -> int:
     return None
 
 def _remove_items(dst: list, removals: list) -> list:
-    positions = {}
+    removal_mapping = {}
     for index, pos in enumerate(removals):
-        if pos is not None: positions.setdefault(pos, []).append(index)
+        if pos is not None: removal_mapping.setdefault(pos, []).append(index)
     count = 0
     removed = [None] * len(removals)
     # For each, remove the item at pos, and update result
     # using indicies where the removal was requested
     # Sort positions in descending order
-    for pos, indexes in sorted(positions.items(), key=lambda item: item[0], reverse=True):
+    for pos, indices in sorted(removal_mapping.items(), key=lambda mapping: mapping[0], reverse=True):
         # Invalid positions are ignored
         if 0 <= pos < len(dst):
             removed_value = dst[pos]
             del dst[pos]
             count += 1
             if removed_value is not None:
-                for i in indexes:
-                    removed[i] = removed_value
+                for index in indices:
+                    removed[index] = removed_value
     return count, removed
