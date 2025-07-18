92944cb47e59cf38432e6ee84010bdef30489304
Ross Virostko <rvirostko@icloud.com>
2025-07-18 10:39:30 -0400

    Rewrite and doc. Two feature changes: 1) allow None as a path (because you can have them as keys!) 2) don't split strings like a path so you can deal with    really messy data without tricks

--- mathpak/attrs.py
+++ mathpak/attrs.py
@@ -4,38 +4,33 @@ Functions for working with attrs and dictionaries
 
 from typing import Any
 
-from .common import type_str, NoneType
+from .common import type_str
 
-def poly_dig(x: Any, *args) -> Any:
-    """Traverse a path in a dictionary object.
+def poly_dig(data: Any, *args) -> Any:
+    """
+**Traverse a path in a dictionary object**
 
-* _dict_.Dig(_path_ [, _path_]) [;]
+* _value_.Dig()
+* _value_.Dig(_path_ [, _path_])
+
+The _value_ must either be a dictionary or _None_.
 
 Path parts can be:
-* A string with periods intoducing subpath components
-* Bool, int, float are converted to single step paths
+* A _None_, str, bool, int, or float
 * A list composed of path components
 """
-    data = x
-    for arg in args:
-        data = _dig(data, arg)
-        if data is None: break
+    if data is None: return data
+    if not isinstance(data, dict): raise TypeError(f'Digging into {type_str(data)} not supported')
+    # Always go through the path parts just to validate them
+    for arg in args: data = _dig(data, arg)
     return data
 
-def _dig(x: Any, path: Any) -> Any:
-    if isinstance(x, (NoneType, bool, int, float, str, tuple, list)): return x
-    if isinstance(x, dict):
-        if isinstance(path, str):
-            # DO NOT STRIP!
-            # Crap headers in CSVs frequently have trailing spaces
-            path = [step for step in path.split('.') if not step.isspace()]
-        elif isinstance(path, (bool, int, float)):
-            path = [path]
-        elif not isinstance(path, (tuple, list)):
-            raise TypeError(f'Digging with a {type_str(path)} not supported')
-        data = x
-        for key in path:
-            if not isinstance(data, dict) or key not in data: return None
-            data = data[key]
-        return data
-    raise TypeError(f'Digging into {type_str(x)} not supported')
+def _dig(data: dict, path: Any) -> Any:
+    if path is None or isinstance(path, (str, bool, int, float)):
+        path = [path]
+    elif not isinstance(path, (tuple, list)):
+        raise TypeError(f'Digging with a {type_str(path)} not supported')
+    for key in path:
+        if not isinstance(data, dict) or key not in data: return None
+        data = data[key]
+    return data
