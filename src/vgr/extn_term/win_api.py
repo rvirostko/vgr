99f16cc6cebaea4b390aab2a7a77ce2506abc1c9
Ross Virostko <rvirostko@icloud.com>
2025-10-03 09:59:44 -0400

    Windows rubbish.

---
+++ src/vgr/extn_term/win_api.py
@@ -0,0 +1,17 @@
+"""
+Winows console functions
+"""
+
+_FIRST_ERR = True
+
+def win_get_cursor_pos() -> list:
+    try:
+        from blessed import Terminal
+        cursor_y_pos, cursor_x_pos = Terminal().get_location()
+        return [cursor_y_pos + 1, cursor_x_pos + 1]
+    except Exception as e:
+        global _FIRST_ERR
+        if _FIRST_ERR:
+            print(e) # TODO log
+            _FIRST_ERR = False
+        return None
