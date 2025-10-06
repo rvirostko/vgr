2481d656d4e29d86468d5e0b5934851c18cc6ea2
Ross Virostko <rvirostko@icloud.com>
2025-10-06 18:57:37 -0400

    Bug fix: None <shift> str causes error Documentation update

--- src/vgr/mathpak/shift.py
+++ src/vgr/mathpak/shift.py
@@ -5,7 +5,15 @@ Bitwise shift left and right.
 from functools import reduce
 from typing import Any
 
-from .common import bound_ops, dist_x, str_to_int, X_None_Op, Y_None_Op, get_operation, matching_default
+from .common import (
+    bound_ops,
+    dist_x,
+    empty_is_zero,
+    get_operation,
+    matching_default,
+    X_None_Op,
+    Y_None_Op,
+)
 
 @bound_ops("<<")
 def poly_shl(x: Any, *args) -> Any:
@@ -33,14 +41,16 @@ integers to perform the operation.
 | list  | int   | list    | distributed          |
 | list  | float | list    | distributed          |
 | list  | str   | list    | distributed          |
-| tuple | int   | tuple   | distributed          |
-| tuple | float | tuple   | distributed          |
-| tuple | str   | tuple   | distributed          |
 
 TypeError raised on all other combinations
 
 ```vgr
-**TODO**
+None << None → None
+None << 2 → 0
+None << "2" → 0
+5 << " 2" → 20
+"5" << "2.0" → 20
+[5, 7] << 2 → [20, 28]
 ```
 
 Also see `RightShift()`
@@ -73,14 +83,16 @@ integers to perform the operation.
 | list  | int   | list    | distributed          |
 | list  | float | list    | distributed          |
 | list  | str   | list    | distributed          |
-| tuple | int   | tuple   | distributed          |
-| tuple | float | tuple   | distributed          |
-| tuple | str   | tuple   | distributed          |
 
 TypeError raised on all other combinations
 
 ```vgr
-**TODO**
+None >> None → None
+None >> 2 → 0
+None >> "2" → 0
+5 >> " 2" → 1
+"5" >> "2.0" → 1
+[5, 7] >> 2 → [1, 1]
 ```
 
 Also see `LeftShift()`
@@ -98,18 +110,18 @@ def _shr(x: Any, y: Any) -> Any:
 shift_operations = {
     X_None_Op: lambda op, _, y: None if y is None else op(matching_default(y), y),
     Y_None_Op: lambda op, x, _: op(x, matching_default(x)),
-    (int, str): lambda op, x, y: op(x, str_to_int(y)),
+    (int, str): lambda op, x, y: op(x, empty_is_zero(y)),
     (int, float): lambda op, x, y: op(x, int(y)),
     (float, int): lambda op, x, y: op(int(x), y),
     (float, float): lambda op, x, y: op(int(x), int(y)),
-    (float, str): lambda op, x, y: op(int(x), str_to_int(y)),
-    (str, int): lambda op, x, y: op(str_to_int(x), y),
-    (str, float): lambda op, x, y: op(str_to_int(x), int(y)),
-    (str, str): lambda op, x, y: op(str_to_int(x), str_to_int(y)),
+    (float, str): lambda op, x, y: op(int(x), empty_is_zero(y)),
+    (str, int): lambda op, x, y: op(empty_is_zero(x), y),
+    (str, float): lambda op, x, y: op(empty_is_zero(x), int(y)),
+    (str, str): lambda op, x, y: op(empty_is_zero(x), empty_is_zero(y)),
     (list, int): dist_x,
     (list, float): lambda op, x, y: dist_x(op, x, int(y)),
-    (list, str): lambda op, x, y: dist_x(op, x, str_to_int(y)),
+    (list, str): lambda op, x, y: dist_x(op, x, empty_is_zero(y)),
     (tuple, int): dist_x,
     (tuple, float): lambda op, x, y: dist_x(op, x, int(y)),
-    (tuple, str): lambda op, x, y: dist_x(op, x, str_to_int(y)),
+    (tuple, str): lambda op, x, y: dist_x(op, x, empty_is_zero(y)),
 }
