6b1ea285728e121fdc270519195d1ff66743688e
Ross Virostko <rvirostko@icloud.com>
2025-10-06 20:40:53 -0400

    Bug fix: sort of strings not working Documentation update.

--- src/vgr/mathpak/general.py
+++ src/vgr/mathpak/general.py
@@ -1,20 +1,25 @@
 from copy import copy
 from functools import cmp_to_key
 from typing import Any
-import sys
 
 from .common import str_to_number, type_str, bool_arg, dist_x
 from .inequ import poly_lt, poly_gt, poly_eq, poly_ne
 
 def poly_reverse(x: Any) -> Any:
     """
-**Reverses the contents of the value**
+**Reverses the contents of the lists and strings**
+
+* Reverse(_value_)
+* _value_.Reverse()
 
 If _value_ is an ordinal rather than a list, it is returned unchanged.
-See the Python [reversed() function](https://docs.python.org/3/library/functions.html#reversed)
 
 ```vgr
-**TODO**
+"five".Reverse() → "evif"
+5.Reverse() → 5
+5.0.Reverse() → 5.0
+["five", 5, 5.0].Reverse() → [5.0, 5, "five"]
+"One Two".Reverse() → "owT nenO"
 ```
 """
     if isinstance(x, (list, tuple)): return list(reversed(x))
@@ -25,11 +30,16 @@ def poly_ascii(x: Any) -> str:
     """
 **Returns a printable ASCII string for an item**
 
-Similar to both Repr() and ToStr().
-See the Python [ascii() function](https://docs.python.org/3/library/functions.html#ascii)
+Similar to both `Repr()` and `ToStr()`.
+Characters outside of ASCII printables are encoded
+with backslash sequences.
 
 ```vgr
-**TODO**
+"five".Ascii() → "five"
+5.Ascii() → 5
+5.0.Ascii() → 5.0
+["five", 5, 5.0].Ascii() → ["five", 5, 5.0]
+"One\\nTwo".Ascii() → "One\\nTwo"
 ```
 """
     return ascii(x)
@@ -38,12 +48,16 @@ def poly_hash(x: Any) -> int:
     """
 **Returns the internal hashcode for an object**
 
+* Hash(_value_)
 * _value_.Hash()
 
 This can be used in debugging but is of limited values in scripts.
+Cannot be applied to lists or dictionaries.
 
 ```vgr
-**TODO**
+"five".Hash() → -3781267357408442496
+5.Hash() → 5
+5.0.Hash() → 5
 ```
 """
     return hash(x)
@@ -52,6 +66,7 @@ def poly_clone(x: Any) -> Any:
     """
 **Ceates a copy of complex objects**
 
+* Clone(_value_)
 * _value_.Clone()
 
 Only lists and dictionaries are cloned, as other
@@ -85,6 +100,8 @@ Print z.Id(), z′.Id()
 Print z.FirstItem().Id(), z′.FirstItem().Id()
 4502192256 4502192256 // same contents
 ```
+
+Also see `Id()` and `Hash()`
 """
     if isinstance(x, (list, dict)): return copy(x)
     return x
@@ -94,71 +111,86 @@ def poly_repr(x: Any) -> str:
 **Returns a string representation of an item
 
 Differs slightly from `ToStr()` as it surrounds string values with single quotes.
-See the Python [repr() function][https://docs.python.org/3/library/functions.html#repr]
-
-Also see `Ascii()`
 
 ```vgr
-**TODO**
+"five".Repr() → "five"
+5.Repr() → 5
+5.0.Repr() → 5.0
+["five", 5, 5.0].Repr() → ["five", 5, 5.0]
 ```
+
+Also see `Ascii()`
 """
     return repr(x)
 
 def poly_type(x: Any) -> str:
     """
-**Return the data type of an item**
+**Return the internal data type of an item**
+
+* Type(_value_)
+* _value_.Type()
 
 For _None_ the value _NoneType_ is returned.
 
 ```vgr
-**TODO**
+None.Type() → "NoneType"
+"five".Type() → "str"
+5.Type() → "int"
+5.0.Type() → "float"
+["five", 5, 5.0].Type() → "list"
+{"One": 1, "Two": 2}.Type() → "dict"
 ```
-
 """
     return type(x).__name__
 
 def poly_sort(x: Any, unique: bool=False, reverse: bool=False) -> Any:
     """
-    Sort with unique and reverse
-    """
+**Sort lists and strings with unique and reverse**
+
+* Sort(_value_)
+* Sort(_value_, _unique_)
+* Sort(_value_, _unique_, _reverse_)
+* _value_.Sort()
+* _value_.Sort(_unique_)
+* _value_.Sort(_unique_, _reverse_)
+
+```vgr
+None.Sort() → None
+5.Sort() → 5
+5.0.Sort() → 5.0
+"dza".Sort() → "adz"
+"dza".Sort(False, True) → "zda"
+[5.1, 5, 5.0].Sort() → [5, 5.0, 5.1]
+["five", 5, 5.0].Sort() → [5, 5.0, "five"]
+```
+"""
     unique = False if unique is None else bool_arg(unique, 'Unique')
     reverse = False if reverse is None else bool_arg(reverse, 'Reverse')
-    if isinstance(x, str): return poly_sort(x.encode(), unique, reverse).decode()
+    if isinstance(x, str):
+        return ''.join(chr(v) for v in poly_sort([ord(ch) for ch in x], unique, reverse))
     if isinstance(x, (list, tuple)):
         rc = list(sorted(x, key=cmp_to_key(_cmp_to_key_asc), reverse=reverse))
         return _unique_sorted(rc) if unique else rc
     return x
 
-def poly_sizeof(x: Any) -> int:
-    """
-**Recursively calculates the size of an item and all its contents**
-
-```vgr
-**TODO**
-```
-
-"""
-    base: int = sys.getsizeof(x)
-    if isinstance(x, (list, tuple, set, dict)): return base + sum(poly_sizeof(x1) for x1 in x)
-    if isinstance(x, dict): return base + sum(poly_sizeof(key) + poly_sizeof(value) for key, value in x.items())
-    return base
-
 def poly_getitem(x:Any, index: Any) -> Any:
     """
 **Return the N-th item from a list**
 
-* _value_.Item(_expression_)
+* Item(_value_, _index_)
+* _value_.Item(_index_)
 
-If the index is invalid or _None_ then _None_ is returned.
-For all other types the value is returned unchanged.
-If the expression itself is a list, the corresponding items
-will be returned in an array.
+For non-list types _value_ is returned unchanged.
+If _index_ itself is a list, the corresponding items
+will be returned in an list. Index values are zero-based.
+
 Requests for items outside the list's bounds results in _None_.
 
 ```vgr
 **TODO**
 ```
 
+Also see `FirstItem()` and `LastItem()`
 """
     if not isinstance(x, (list, tuple)): return x
     if isinstance(index, (list, tuple)): return dist_x(poly_getitem, x, index)
@@ -169,14 +201,17 @@ def poly_firstitem(x: Any) -> Any:
     """
 **Return the first item from a list**
 
+* FirstItem(_value_)
 * _value_.FirstItem()
 
 If the list is empty then _None_ is returned.
-For all other types the value is returned unchanged.
+For non-list types _value_ is returned unchanged.
 
 ```vgr
 **TODO**
 ```
+
+Also see `Item()` and `LastItem()`
 """
     return poly_getitem(x, 0)
 
@@ -184,14 +219,17 @@ def poly_lastitem(x: Any) -> Any:
     """
 **Return the last item from a list**
 
+* LastItem(_value_)
 * _value_.LastItem()
 
 If the list is empty then _None_ is returned.
-For all other types the value is returned unchanged.
+For non-list types _value_ is returned unchanged.
 
 ```vgr
 **TODO**
 ```
+
+Also see `Item()` and `FistItem()`
 """
     if not isinstance(x, (list, tuple)): return x
     return x[-1] if len(x) > 0 else None
@@ -200,6 +238,7 @@ def poly_unique(x: Any) -> Any:
     """
 **A unique that works with lists or strings**
 
+* Unique(_value_)
 * _value_.Unique()
 
 For strings, a string containing all the unique characters in
