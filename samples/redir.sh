06b35319d79f751a02d0037764c64664e34d6f11
Ross Virostko <rvirostko@icloud.com>
2025-10-14 10:43:12 -0400

    Updated for new arg passing syntax

--- samples/redir.sh
+++ samples/redir.sh
@@ -3,39 +3,41 @@
 temp_file=$(mktemp --dry-run ./tmpfile.XXXXXX)
 
 printf "temp_file=%s\n" "${temp_file}"
+export temp_file
 
 # Test that we can create the file and it contains what we expect
-../vgr.py "temp.file=${temp_file}" <<EOF || echo "FAILED"
+../vgr.py <<EOF || echo "FAILED"
 Print "\n--Output Overwrite Test"
+
 Echo True
-Exhibit arg.temp.file
-Open Output arg.temp.file
-Printf "{}", arg.temp.file
+Exhibit env.temp_file
+Open Output env.temp_file
+Printf "{}", env.temp_file
 Close Output
-Load arg.output From arg.temp.file
+Load arg.output From env.temp_file
 Exhibit arg.output
-Assert arg.output == arg.temp.file
+Assert arg.output == env.temp_file
 EOF
 
 # Make sure overwrite protection is working
-../vgr.py "temp.file=${temp_file}" \
+../vgr.py \
     -e 'Print "\n--Output No Overwrite Test"' \
     -e 'Echo True' \
-    -e 'Open Output arg.temp.file No Overwrite' \
+    -e 'Open Output env.temp_file No Overwrite' \
     && echo "Overwrite protected FAILED"
 
 # Test out appending
-../vgr.py "temp.file=${temp_file}" <<EOF || echo "FAILED"
+../vgr.py <<EOF || echo "FAILED"
 Print "\n--Output Append Test"
 Echo True
-Exhibit arg.temp.file
-Open Output arg.temp.file Append
-Printf "{}", arg.temp.file
+Exhibit env.temp_file
+Open Output env.temp_file Append
+Printf "{}", env.temp_file
 Close Output
-Load arg.output From arg.temp.file
+Load arg.output From env.temp_file
 Exhibit arg.output
 # One copy from our first test, another from this one
-Assert arg.output == (arg.temp.file * 2)
+Assert arg.output == (env.temp_file * 2)
 EOF
 
 # Make sure only relative paths can be used
@@ -48,8 +50,8 @@ EOF
 rm -rf "$temp_file"
 
 # Test mkdir -p like functionality
-../vgr.py "temp.file=${temp_file}" \
-    -e 'Print "\n--Create subdirs Test"; Open Output arg.temp.file + "/sub1/sub2/out.txt"' \
+../vgr.py \
+    -e 'Print "\n--Create subdirs Test"; Open Output env.temp_file + "/sub1/sub2/out.txt"' \
     || echo "Subdir creating FAILED"
 
 ls -lR "$temp_file"
@@ -60,8 +62,8 @@ touch "$temp_file/sub1"
 ls -lR "$temp_file"
 
 # Test file blocking dir creation
-../vgr.py "temp.file=${temp_file}" \
-    -e 'Print "\n--Blocking Test"; Open Output arg.temp.file + "/sub1/sub2/out.txt"' \
+../vgr.py \
+    -e 'Print "\n--Blocking Test"; Open Output env.temp_file + "/sub1/sub2/out.txt"' \
     && echo "Blocking File FAILED"
 
 rm -rf "$temp_file"
