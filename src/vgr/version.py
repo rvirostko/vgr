953d5eef947dc9102b805757d76b1b009897ea20
Ross Virostko <rvirostko@icloud.com>
2025-10-24 17:35:20 -0400

    Bump to next version

--- src/vgr/version.py
+++ src/vgr/version.py
@@ -3,7 +3,7 @@ Version information
 """
 
 # These are maintained by bump_version.sh
-__version__ = "1.1.5"
+__version__ = "1.1.6"
 __version_date__ = "2025-10-24"
 
 __description__ = "A scripting language for report writing"
