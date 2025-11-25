8672c98d0a2e92ace5ee395fe8867420e7c7248e
Ross Virostko <rvirostko@icloud.com>
2025-11-25 14:59:03 -0500

    Update version date

--- src/vgr/__init__.py
+++ src/vgr/__init__.py
@@ -1,7 +1,7 @@
 
 # These are maintained by bump_version.sh
 __version__ = "1.1.8"
-__version_date__ = "2025-11-17"
+__version_date__ = "2025-11-25"
 
 __description__ = "A scripting language for report writing"
 
