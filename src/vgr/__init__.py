b9ccec4bf935702ef7f89392ef5acc65ecc942d6
Ross Virostko <rvirostko@icloud.com>
2026-02-12 09:58:33 -0500

    Start on v1.1.10

--- src/vgr/__init__.py
+++ src/vgr/__init__.py
@@ -1,7 +1,7 @@
 
 # These are maintained by bump_version.sh
-__version__ = "1.1.9"
-__version_date__ = "2026-02-10"
+__version__ = "1.1.10"
+__version_date__ = "2026-02-12"
 
 __description__ = "A scripting language for report writing"
 
