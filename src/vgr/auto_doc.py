b4997b82cedd7a610d2247ebd08282ddd5540ad6
Ross Virostko <rvirostko@icloud.com>
2026-02-18 14:39:40 -0500

    Missed: Generate Doc from external... this does all the work

--- src/vgr/auto_doc.py
+++ src/vgr/auto_doc.py
@@ -4,6 +4,7 @@ Create a Markdown file from internal source
 
 from pathlib import Path
 import re
+import sys
 
 from . import __version__, __version_date__
 from .functions import (
@@ -22,26 +23,63 @@ def gen_auto_docs() -> None:
     _OPERATORS.extend(get_operator_entries().keys())
     path = Path("vgr-" + __version__ + ".md")
     with path.open("w", encoding="utf-8", newline="\n") as f:
-        _create_cover_page(f)
+        _write_cover_page(f)
         _write_toc(f)
+        _write_running(f)
+        _write_repl(f)
+        _write_variables(f)
         _write_statements(f)
         _write_operators(f)
         _write_functions(f)
 
+def _write_cover_page(f) -> None:
+    f.write(_read_doc_file("cover_pg.md").format(__version__=__version__, __version_date__=__version_date__))
+
 def _write_toc(f) -> None:
-    # TODO needs a "variables" overview
-    f.write(f"""
-<div style="page-break-before: always; break-before: page;"></div>
+    _write_page_break(f)
+    _write_anchor("toc", f)
+    _copy_doc_file("toc.md", f)
 
-## CONTENTS
+def _write_running(f) -> None:
+    _write_page_break(f)
+    _write_anchor("chapter-running", f)
+    _copy_doc_file("running.md", f)
 
-### {_link("Statements", "chapter-statements")}
+def _write_repl(f) -> None:
+    _write_page_break(f)
+    _write_anchor("chapter-repl", f)
+    _copy_doc_file("repl.md", f)
 
-### {_link("Operators", "chapter-operators")}
+def _write_variables(f) -> None:
+    _write_page_break(f)
+    _write_anchor("chapter-variables", f)
+    _copy_doc_file("variables.md", f)
 
-### {_link("Functions", "chapter-functions")}
+def _write_statements(f) -> None:
+    _write_page_break(f)
+    _write_anchor("chapter-statements", f)
+    _copy_doc_file("statements.md", f)
+    _write_all_doc(f, get_statement_entries(), 'statement-')
 
-""")
+def _write_functions(f) -> None:
+    _write_page_break(f)
+    _write_anchor("chapter-functions", f)
+    _copy_doc_file("functions.md", f)
+    _write_all_doc(f, get_function_entries(), 'function-', True)
+
+def _write_operators(f) -> None:
+    _write_page_break(f)
+    _write_anchor("chapter-operators", f)
+    _copy_doc_file("operators.md", f)
+    _write_all_doc(f, get_operator_entries(), 'operator-')
+
+def _write_page_break(f) -> None:
+    f.write('\n<div style="page-break-before: always; break-before: page;"></div>\n')
+
+def _write_anchor(anchor_id: str, f) -> None:
+    f.write("\n")
+    f.write(_anchor(anchor_id))
+    f.write("\n\n")
 
 def _alpha_first_key(s: str):
     """
@@ -49,21 +87,6 @@ def _alpha_first_key(s: str):
     Default ordering is preserved within each group.
     """
     return (not s[:1].isalpha(), s)
-
-def _write_statements(f) -> None:
-    f.write(_heading("STATEMENTS", "chapter-statements"))
-    # TODO blurb from file
-    _write_all_doc(f, get_statement_entries(), 'statement-')
-
-def _write_functions(f) -> None:
-    f.write(_heading("FUNCTIONS", "chapter-functions"))
-    # TODO blurb from file
-    _write_all_doc(f, get_function_entries(), 'function-', True)
-
-def _write_operators(f) -> None:
-    f.write(_heading("OPERATORS", "chapter-operators"))
-    # TODO blurb from file
-    _write_all_doc(f, get_operator_entries(), 'operator-')
 
 def _write_all_doc(f, entries: dict, anchor_prefix: str, is_function: bool=False) -> None:
     names = list(entries.keys())
@@ -80,6 +103,7 @@ def _write_all_doc(f, entries: dict, anchor_prefix: str, is_function: bool=False
                 mk_links = mk_links or line.strip().startswith('Also see')
                 if mk_links: line = re.sub(r"`([^`]+)`", _expand_hyperlink, line)
                 doc += line + '\n'
+                doc = doc.lstrip()
             heading = f'`{name}{"()" if is_function else ""}` - {title}'
         else:
             heading = f'`{name}{"()" if is_function else ""}`'
@@ -94,20 +118,19 @@ def _write_all_doc(f, entries: dict, anchor_prefix: str, is_function: bool=False
             # write a link to primary
             # cannot make assumption about anchor prefix: see Add()
             f.write("See " + _link(f'`{primary_name}`', _anchor_for(primary_name)))
-        f.write('\n</div>\n\n')
+        f.write('\n</div>\n')
 
-def _create_cover_page(f) -> None:
-    f.write(f"""
-<div align="center" style="padding: 20rem 0 30rem;">
+def _read_doc_file(src_filename: str) -> str:
+    module_path = Path(__file__).resolve()
+    src_file = Path(module_path.parent / "doc" / src_filename)
+    if src_file.is_file():
+        with open(src_file, 'r', encoding='utf-8') as file_in:
+            return file_in.read().rstrip() + '\n'
+    print(f"Did not find {src_file}", file=sys.stdout)
+    return ''
 
----
-<div style="font-size: 200%">VGR LANGUAGE REFERENCE</div>
-<div style="font-size: 75%">Version {__version__} • {__version_date__}</div>
-
----
-</div>
-
-""")
+def _copy_doc_file(src_filename: str, output) -> None:
+    output.write(_read_doc_file(src_filename))
 
 def _anchor_for(text: str) -> str:
     if text.endswith("()"): return "function-" + text[:-2].casefold()
