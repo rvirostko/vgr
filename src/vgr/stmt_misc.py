4da56d0151a4afe7fffcdaa086b83b8d8a468145
Ross Virostko <rvirostko@icloud.com>
2026-04-07 13:07:57 -0400

    Documentation for Sleep

--- src/vgr/stmt_misc.py
+++ src/vgr/stmt_misc.py
@@ -6,7 +6,10 @@ import time
 
 from lark import Tree
 
-from .builtins import bound_ops
+from .builtins import (
+    bound_ops,
+    poly_plural,
+)
 from .exec_context import ExecContext
 
 @bound_ops("Sleep")
@@ -16,18 +19,47 @@ def execute_sleep(ctx: ExecContext, statement: Tree) -> None:
 
 * Sleep [For] *expression* [Second | Seconds] [;]
 
-Values may be floating point, e.g. .01 to delay for ten milliseconds.
-Negative and zero values are ignored. Maximum sleep time is five minutes.
+Sleep time is a number. Strings are converted to number if possible
+Floating point values are allowed, e.g. .01 to delay for ten milliseconds.
+`None`, negative, and zero values are ignored.
+
+The maximum sleep time is 300 seconds or five minutes.
 
 ```vgr
-**TODO**
+Verbose
+Verbose = True
+Sleep None
+Sleep skipped
+
+Sleep Zero
+Sleep skipped
+
+Sleep 2
+Sleeping for 2 seconds
+
+Sleep 2.75
+Sleeping for 2.75 seconds
+
+Sleep For .01 Seconds
+Sleeping for 10 ms
+
+Sleep For 1/3 Seconds
+Sleeping for 333.333 ms
 ```
 
 """
-    n = ctx.eval_to_number(statement.children[0], 'Sleep time')
-    n = min(max(n, 0), 300)
-    if n > 0:
-        ctx.print_verbose('Sleeping for', n, 'seconds')
-        time.sleep(n)
-    else:
-        ctx.print_verbose('Sleep skipped')
+    def fmt(x: float) -> str:
+        if x.is_integer(): return str(int(x))
+        return f"{x:.3f}".rstrip("0").rstrip(".")
+    n = ctx.eval_to_number(statement.children[0], 'Sleep time', True)
+    if n is not None:
+        n = min(max(n, 0), 300)
+        if n > 0:
+            if ctx.verbose:
+                if n >= 1:
+                    ctx.print_verbose('Sleeping for', fmt(n), poly_plural(n, 'seconds', 'second'))
+                else:
+                    ctx.print_verbose('Sleeping for', fmt(n * 1000), 'ms')
+            time.sleep(n)
+            return
+    ctx.print_verbose('Sleep skipped')
