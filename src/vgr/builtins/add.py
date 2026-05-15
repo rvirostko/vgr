f87c275cbc5e795930621b890573567f6e34c0b1
Ross Virostko <rvirostko@icloud.com>
2026-05-15 11:18:53 -0400

    Increase the number of types that can be combined with "+" (all non-numeric operations), mostly with Pattern and dictionary. Added better error handling on TypeErrors.

--- src/vgr/builtins/add.py
+++ src/vgr/builtins/add.py
@@ -1,7 +1,17 @@
 from functools import reduce
+from re import Pattern
 from typing import Any
 
-from .common import bound_ops, get_operation, numeric_operations, dist_x, dist_y, str_to_number
+from .common import (
+    bound_ops,
+    get_operation,
+    numeric_operations,
+    dist_x,
+    dist_y,
+    str_to_number,
+)
+from .dict import poly_set_key_value
+from .type import poly_type
 
 @bound_ops("+", "＋")
 def poly_add(*args):
@@ -49,25 +59,56 @@ None + "5" → "5"
 ```
 
 Also see `ToNumber()` and `ToString()` for conversion details
-
 """
     return reduce(_add, args[1:], args[0]) if args else None
 
 def _add(x: Any, y: Any) -> Any:
     operation = get_operation(x, y, _add_operations, numeric_operations)
-    return operation(_add, x, y) if operation else x + y
+    try:
+        return operation(_add, x, y) if operation else x + y
+    except TypeError as e:
+        raise TypeError(f"Cannot add types {poly_type(x)!r} and {poly_type(y)!r}") from e
+
+def _add_key(_op, d: dict, key: Any) -> dict: return poly_set_key_value(d, key)
+def op_pattern_str(op, pattern: Pattern, value: Any) -> str:
+    """Convert both values to a string and invoke op()"""
+    return op(pattern.pattern, str(value))
+def op_str_pattern(op, value: Any, pattern: Pattern) -> str:
+    """Convert both values to a string and invoke op()"""
+    return op(str(value), pattern.pattern)
+def op_str_str(op, x: Any, y: Any) -> str:
+    """Convert both values to a string and invoke op()"""
+    return op(str(x), str(y))
 
 _add_operations = {
-    (int, list): dist_y,
-    (float, list): dist_y,
-    (str, str): lambda _, x, y: x + y,
-    (str, int): lambda _, x, y: x + str(y),
-    (str, bool): lambda _, x, y: x + str(y),
-    (str, float): lambda _, x, y: x + str(y),
-    (str, list): dist_y,
-    (list, int): dist_x,
-    (list, float): dist_x,
-    (dict, dict): lambda _, x, y: {**x, **y}
+    (bool, Pattern):    op_str_pattern,
+    (int, list):        dist_y,
+    (int, Pattern):     op_str_pattern,
+    (float, list):      dist_y,
+    (float, Pattern):   op_str_pattern,
+    (str, bool):        op_str_str,
+    (str, int):         op_str_str,
+    (str, float):       op_str_str,
+    (str, str):         lambda _, x, y: x + y,
+    (str, list):        dist_y,
+    (str, Pattern):     op_str_pattern,
+    (list, bool):       dist_x,
+    (list, int):        dist_x,
+    (list, float):      dist_x,
+    (list, Pattern):    lambda op, x, y: dist_x(op, x, y.pattern),
+    (dict, bool):       _add_key,
+    (dict, int):        _add_key,
+    (dict, float):      _add_key,
+    (dict, str):        _add_key,
+    (dict, list):       _add_key,
+    (dict, dict):       lambda _, x, y: {**x, **y},
+    (dict, Pattern):    lambda op, x, y: _add_key(op, y, x.pattern),
+    (Pattern, bool):    op_pattern_str,
+    (Pattern, int):     op_pattern_str,
+    (Pattern, float):   op_pattern_str,
+    (Pattern, str):     op_pattern_str,
+    (Pattern, list):    lambda op, x, y: dist_y(op, x.pattern, y),
+    (Pattern, Pattern): lambda _, x, y: x.pattern + y.pattern,
 }
 
 def poly_sum(*args) -> Any:
@@ -83,7 +124,7 @@ String values are converted to numbers when possible.
 
 While similar to `Add()`, `Sum()` is not distributed over
 lists, but instead sums their content.
-Additionally, if strings cannot be converted to a number
+Additionally, if strings, or other types, cannot be converted to a number
 they are ignored.
 
 ```vgr
@@ -97,15 +138,18 @@ True.Sum(True, None, True) → 3
 ["cat", "dog", "fish"].Sum() → 0
 ```
 """
-    return _sum(args[0]) + sum(_sum(arg) for arg in args[1:]) if args else 0
+    # NB: _num_value should prevent any TypeError in the addition
+    return _num_value(args[0]) + sum(_num_value(arg) for arg in args[1:]) if args else 0
 
-def _sum(obj):
+def _num_value(obj):
     if obj is None: return 0
     if isinstance(obj, (int, float)): return obj
+    # Likely "unuseful" but is consistent
+    if isinstance(obj, Pattern): obj = obj.pattern
     if isinstance(obj, str):
         try:
             return str_to_number(obj) or 0
         except ValueError:
             pass
-    if isinstance(obj, list): return sum(_sum(item) for item in obj)
+    if isinstance(obj, list): return sum(_num_value(item) for item in obj)
     return 0
