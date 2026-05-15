7672d09ddcd85fb03f64553b358a865c5ba46223
Ross Virostko <rvirostko@icloud.com>
2026-05-15 11:38:16 -0400

    Mostly minor changes so we have better error messages, some (admitedly useless) support for pattern, and key removal matches what addition does.

--- src/vgr/builtins/sub.py
+++ src/vgr/builtins/sub.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from re import Pattern
 from typing import Any
 
 from .common import (
@@ -7,6 +8,8 @@ from .common import (
     get_operation,
     numeric_operations,
 )
+from .dict import poly_remove_key
+from .type import poly_type
 
 @bound_ops("-", "－")
 def poly_sub(*args):
@@ -33,7 +36,7 @@ def poly_sub(*args):
 | list       | float      | list          | Distributed               |
 | list       | string     | list          | Distributed               |
 | dictionary | string     | dictionary    | Remove key y from x       |
-| dictionary | list       | dictionary    | Remove keys in y from x   |
+| dictionary | list       | dictionary    | Remove key path y from x  |
 | dictionary | dictionary | dictionary    | Remove keys in y from x   |
 
 A type error is raised on all other combinations
@@ -53,17 +56,28 @@ None - "5" → -5
     return reduce(_sub, args[1:], args[0]) if args else None
 
 def _sub(x: Any, y: Any) -> Any:
-    operation = get_operation(x, y, sub_operations, numeric_operations)
-    return operation(_sub, x, y) if operation else x - y
+    operation = get_operation(x, y, _sub_operations, numeric_operations)
+    try:
+        return operation(_sub, x, y) if operation else x - y
+    except TypeError as e:
+        raise TypeError(f"Cannot substract type {poly_type(y)!r} from {poly_type(x)!r}") from e
 
-def remove_keys(x: dict, y: Any) -> dict:
-    return {k:v for k, v in x.items() if k not in y}
+def _remove_key(_op, x: dict, path: Any) -> dict:
+    """Remove a key using a path"""
+    return poly_remove_key(x, path)
 
-sub_operations = {
-    (str, str): lambda op, x, y: op(empty_is_zero(x), empty_is_zero(y)),
-    (dict, int): lambda _, x, y: remove_keys(x, [y]),
-    (dict, float): lambda _, x, y: remove_keys(x, [y]),
-    (dict, str): lambda _, x, y:  remove_keys(x, [y]),
-    (dict, list): lambda _, x, y: remove_keys(x, y),
-    (dict, dict): lambda _, x, y:  remove_keys(x, list(y.keys())),
+def _remove_keys(_op, x: dict, y: dict) -> dict:
+    """Remove all the keys present in y from x (disjunction)"""
+    keys = list(y.keys())
+    return {k:v for k, v in x.items() if k not in keys}
+
+_sub_operations = {
+    (str, str):      lambda op, x, y: op(empty_is_zero(x), empty_is_zero(y)),
+    (dict, bool):    _remove_key,
+    (dict, int):     _remove_key,
+    (dict, float):   _remove_key,
+    (dict, str):     _remove_key,
+    (dict, list):    _remove_key,
+    (dict, dict):    _remove_keys,
+    (dict, Pattern): lambda op, x, y: _remove_key(op, x, y.pattern)
 }
