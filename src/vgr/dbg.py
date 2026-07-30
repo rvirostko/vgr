582203c1882c7df67890388545151d15e2331405
Ross Virostko <rvirostko@icloud.com>
2026-07-30 15:07:39 -0400

    More defensive programming agains missing line info.

--- src/vgr/dbg.py
+++ src/vgr/dbg.py
@@ -11,26 +11,25 @@ from .redir import print_stderr
 
 def print_tree(item: Any, indent=2) -> None:
     """Prints the parse tree including position information"""
+    def _fmt_pos(meta) -> str:
+        if meta:
+            values = tuple(getattr(meta, attr, None) for attr in ("line", "column", "end_line", "end_column"))
+            if None not in values:
+                return ' (Pos: {}:{}-{}:{})'.format(*values)
+        return ''
     prefix = ' ' * indent  # Indentation for nested levels
     if isinstance(item, Tree):
         tree: Tree = item
         op = getattr(tree, "op_name", lambda: "")()
-        meta = tree.meta
-        if meta:
-            line = getattr(meta, "line", "?")
-            column = getattr(meta, "column", "?")
-            end_line = getattr(meta, "end_line", "?")
-            end_column = getattr(meta, "end_column", "?")
-            pos_info = f' (Pos: {line}:{column}-{end_line}:{end_column})'
-        else:
-            pos_info = ''
-        print_stderr(f'{prefix}({tree.data}:{op}{pos_info}', end=('\n' if tree.children else ''))
+        pos_info = _fmt_pos(tree.meta)
+        print_stderr(f'{prefix}({tree.data}:{op}{pos_info}',
+                     end=('\n' if tree.children else ''))
         for child in tree.children: print_tree(child, indent + 2)
         print_stderr(f'{prefix if tree.children else ""})')  # close the rule
     else:
         if isinstance(item, Token):
             token: Token = item
-            print_stderr(f'{prefix}{token.type}: {token.value!r} '
-                        f'(Pos: {token.line}:{token.column}-{token.end_line}:{token.end_column} {poly_type(token.value)!r})')
+            pos_info = _fmt_pos(token)
+            print_stderr(f'{prefix}{token.type}: {token.value!r} {poly_type(token.value)!r}{pos_info}')
         else:
             raise ValueError(item.type()) # SNO: What else can there be?
