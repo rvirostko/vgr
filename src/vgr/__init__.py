4fb31dada0455b65e446f3027ee0ff4a69c209aa
Ross Virostko <rvirostko@icloud.com>
2026-09-18 11:23:49 -0400

    Bump release date

--- src/vgr/__init__.py
+++ src/vgr/__init__.py
@@ -1,7 +1,7 @@
 
 # These are maintained by bump_version.sh
 __version__ = "1.2.2"
-__version_date__ = "2026-08-06"
+__version_date__ = "2026-09-18"
 
 __description__ = "A scripting language for report writing"
 
